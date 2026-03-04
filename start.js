ec29b568955e76af3a7720da1b493ec530a2e0bd
RamClaw Agent <ramclaw@sandbox.local>
2026-03-04 14:22:49 -0500

    Wait for server ready log before setting local URL

--- start.js
+++ start.js
@@ -3,12 +3,6 @@
 module.exports = {
   run: [
     {
-      method: 'local.set',
-      params: {
-        url: 'http://localhost:3000/'
-      }
-    },
-    {
       when: "{{!exists('sandbox/config.json')}}",
       method: 'shell.run',
       params: {
@@ -23,7 +17,18 @@ module.exports = {
       params: {
         message: [
           'node ramclaw_server.js'
-        ]
+        ],
+        on: [{
+          event: '/RamClaw UI running at http:\\/\\/localhost:\\d+/',
+          done: true
+        }]
+      }
+    },
+    {
+      when: "{{exists('sandbox/config.json')}}",
+      method: 'local.set',
+      params: {
+        url: 'http://localhost:3000/'
       }
     }
   ]
