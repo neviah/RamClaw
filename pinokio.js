8d688a1146c4d058c690b36ea4457d84efcaba7d
RamClaw Agent <ramclaw@sandbox.local>
2026-03-04 14:05:36 -0500

    Revert "Remove timestamp query params from Pinokio menu links"
    This reverts commit 96c1824f7e3cd07865c49852ee86bcebce64e3d5.

--- pinokio.js
+++ pinokio.js
@@ -37,7 +37,7 @@ module.exports = {
 						default: true,
 						icon: 'fa-solid fa-rocket',
 						text: 'OpenWebUI',
-						href: local.url
+						href: local.url + '?ts=' + Date.now()
 					}, {
 						icon: 'fa-solid fa-terminal',
 						text: 'Terminal',
@@ -67,7 +67,7 @@ module.exports = {
 					default: true,
 					icon: 'fa-solid fa-power-off',
 					text: 'Start',
-					href: 'start.js'
+					href: 'start.js?ts=' + Date.now()
 				},
 				{
 					icon: 'fa-solid fa-plug',
